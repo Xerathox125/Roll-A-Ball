6d2813453cc3a9edbb2ae103ab33bc946dcf3826
Xerathox <joaquinaguirrep125@gmail.com>
2026-05-19 15:15:47 -0500

    Movimiento y salto incorporados

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour
     Rigidbody rb;
     public float velocidad;
     float moveX, moveY;
+    public float jumpForce = 5f;
+    public int maxJumps = 2;
+    private int countJump = 0;
+    private bool isGrounded;
+    public float airControl = 0.75f;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,8 +28,17 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 movimiento = new Vector3(moveX, 0, moveY)  * velocidad;
-        rb.AddForce(movimiento);
+        Vector3 movimiento = new Vector3(moveX, 0, moveY) * velocidad;
+        if (isGrounded)
+        {
+            rb.AddForce(movimiento);
+        }
+        else
+        {
+            rb.AddForce(movimiento * airControl);
+
+        }
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x * 0.9f, rb.linearVelocity.y, rb.linearVelocity.z * 0.9f);
     }
 
     void OnMove(InputValue moveValue)
@@ -31,6 +46,41 @@ public class PlayerController : MonoBehaviour
         Vector2 move = moveValue.Get<Vector2>();
         moveX = move.x;
         moveY = move.y;
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Point"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            countJump = 0;
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnJump(InputValue jumpValue)
+    {
+        if (countJump < maxJumps)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+            countJump++;
+        }
     }
 }
+
